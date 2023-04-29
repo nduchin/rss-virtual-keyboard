b88f5040eb84d0e5bc55aaa6f589ae15efdf05cd
Nduchin <nduchin@mail.ru>
2023-04-29 12:36:00 +0300

    feat: set-up webpack, eslint

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const baseConfig = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/app/index.html',
+      filename: 'index.html',
+    }),
+    /*new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets'),
+          to: path.resolve(__dirname, 'dist', 'assets'),
+        },
+      ],
+    }),*/
+    new CleanWebpackPlugin(),
+  ],
+  /*optimization: {
+    splitChunks: {
+      name: 'common',
+      chunks: 'all',
+    },
+  },*/
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+};
+
+module.exports = ({},{ mode })=>{
+  const isProdMode = mode === 'production';
+  const envConfig = isProdMode ? require('./webpack.prod.config') : require('./webpack.dev.config')
+
+  return merge(baseConfig, envConfig);
+}
---
+++ webpack.deploy.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  output: {
+    publicPath: "/virtual-keyboard/",
+  },
+  mode: 'production',
+};
